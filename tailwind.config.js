4145a802e5575548229c06d89e0797f0f25fbebf
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-11-25 08:35:26 +0500

    Add initiol commit

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      // backgroundImage:{
+      //   'hero-pattern':"url('/image/that-logo.svg')",
+      // }
+      backgroundImage: {
+        'hero-pattern': "url('/image/header-bg.jpg')",
+        'night1': "url('/image/night1.jpg')",
+        'hero-pattern2': "url('/image/night2.jpg')",
+        'hero-pattern3': "url('/image/night3.jpg')",
+        'hero-pattern4': "url('/image/night4.jpg')",
+        'hero-pattern5': "url('/image/night5.jpg')",
+        'hero-pattern6': "url('/image/night6.jpg')",
+        'hero-pattern7': "url('/image/night7.jpg')",
+        'hero-pattern8': "url('/image/night8.jpg')",
+        'hero-menu1': "url('/image/menu1.svg')",
+      },
+
+
+
+  //  backgroundImage2: {
+  //       'night1': "url('/image/night1.jpg')",
+  //     },
+      // backgroundImage2: {
+      //   'hero-pattern2': "url('/image/night2.jpg')",
+      // },
+      // backgroundImage3: {
+      //   'hero-pattern3': "url('/image/night3.jpg')",
+      // },
+      // backgroundImage4: {
+      //   'hero-pattern4': "url('/image/night4.jpg')",
+      // },
+      backgroundImage5: {
+      },
+      backgroundImage6: {
+      },
+      backgroundImage7: {
+      },
+      backgroundImage8: {
+      },
+    },
+    plugins: [],
+  }
+}
