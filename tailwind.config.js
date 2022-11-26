f367c7c22d34cc593ec6dd8966eaa39baca43a9b
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-11-26 09:40:10 +0500

    Add init image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,17 @@ module.exports = {
       //   'hero-pattern':"url('/image/that-logo.svg')",
       // }
       backgroundImage: {
-        'hero-pattern': "url('/image/header-bg.jpg')",
-        'night1': "url('/image/night1.jpg')",
-        'hero-pattern2': "url('/image/night2.jpg')",
-        'hero-pattern3': "url('/image/night3.jpg')",
-        'hero-pattern4': "url('/image/night4.jpg')",
-        'hero-pattern5': "url('/image/night5.jpg')",
-        'hero-pattern6': "url('/image/night6.jpg')",
-        'hero-pattern7': "url('/image/night7.jpg')",
-        'hero-pattern8': "url('/image/night8.jpg')",
-        'hero-menu1': "url('/image/menu1.svg')",
+        'hero-pattern':"url('/image/header-bg.jpg')",
+       
+        'night1':"url('/image/night1.jpg')",
+        'hero-pattern2':"url('/image/night2.jpg')",
+        'hero-pattern3':"url('/image/night3.jpg')",
+        'hero-pattern4':"url('/image/night4.jpg')",
+        'hero-pattern5':"url('/image/night5.jpg')",
+        'hero-pattern6':"url('/image/night6.jpg')",
+        'hero-pattern7':"url('/image/night7.jpg')",
+        'hero-pattern8':"url('/image/night8.jpg')",
+        'hero-menu1':"url('/image/menu1.svg')",
       },
 
 
